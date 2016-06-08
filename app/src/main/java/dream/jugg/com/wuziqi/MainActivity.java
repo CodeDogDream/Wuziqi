96f913506e1322cf857bdc95b5e1ae6a5e676986
jugg0109 <503963720@qq.com>
2016-06-08 18:17:49 +0800

    Initial commit

---
+++ app/src/main/java/dream/jugg/com/wuziqi/MainActivity.java
@@ -0,0 +1,33 @@
+package dream.jugg.com.wuziqi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private  WuziqiPanel wuziqiPanel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wuziqiPanel= (WuziqiPanel) findViewById(R.id.my_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id == R.id.action_settings){
+            wuziqiPanel.restart();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
